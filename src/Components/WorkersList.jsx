eb1e185d4c9eccc119ec847ad54722c127041038
Nandaishore <nandakishore17072000@gmail.com>
2023-06-23 12:12:14 -0700

    PendingFromWorkersDash

--- src/Components/WorkersList.jsx
+++ src/Components/WorkersList.jsx
@@ -4,42 +4,41 @@ import requests from '../api/requests.js'
 
 function WorkersList({ column, data }) {
 
+
   const [sessionData, setSessionData] = useState('')
 
   const [selectedRow, setSelectedRow] = useState(null);
+
+  console.log(selectedRow);
 
   const handleClick = (index) => {
     setSelectedRow(index);
   };
 
   useEffect(() => {
-    // to get all sessions from the server
-
-    const URL = "/session/active"
+    const URL = "/session/active";
     const fetchData = async () => {
       try {
-          const response = await requests.getData(URL);
-          if (response.status === 200) {
-              console.log(response.data.data);
-              setSessionData(response.data.data)
-          } else {
-              toast.error(response);
-          }
+        const response = await requests.getData(URL);
+        if (response.status === 200) {
+          console.log(response.data.data);
+          setSessionData(response.data.data);
+        } else {
+          toast.error(response);
+        }
       } catch (error) {
-          toast.error('Something went wrong!');
-          console.log(error);
+        toast.error('Something went wrong!');
+        console.log(error);
       }
-  };
+    };
 
-  fetchData();
+    fetchData();
+  }, []);
 
-  })
-
-  console.log(sessionData);
 
   return (
     <div className="p-5 m-5 relative overflow-x-auto shadow-md sm:rounded-lg bg-opacity-50 backdrop-blur-sm">
-     <Toaster position='top-center' reverseOrder={false}></Toaster>
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
       <table className="min-w-full rounded-xl border-separate border-spacing-y-4">
         <thead className="text-gray-200">
           <tr>
@@ -51,23 +50,25 @@ function WorkersList({ column, data }) {
           </tr>
         </thead>
         <tbody className="text-gray-700">
-          {data.map((rowData, index) => (
-            <tr
-              key={index}
-              className={`text-center h-14 rounded-md border-b shadow-[0px_0px_14px_3px_#00000024] ${
-                selectedRow === index
-                  ? 'bg-gradient-to-r from-indigo-500 to-sky-500 text-white'
-                  : 'bg-slate-100'
-              }`}
-              onClick={() => handleClick(index)}
-            >
-              {column.map((col, columnIndex) => (
-                <td key={columnIndex} className="font-semibold">
-                  {rowData[col.value]}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {data.map((rowData, index) => {
+            const isMatchingSession = rowData._id === sessionData;
+            const rowClassName = `text-center h-14 rounded-md border-b shadow-[0px_0px_14px_3px_#00000024] ${selectedRow === index ? 'bg-gradient-to-r from-indigo-500 to-sky-500 text-white' : 'bg-slate-100'
+              } ${isMatchingSession ? 'bg-gradient-to-r from-green-500 to-emerald-500' : ''}`;
+
+            return (
+              <tr
+                key={index}
+                className={rowClassName}
+                onClick={() => handleClick(index)}
+              >
+                {column.map((col, columnIndex) => (
+                  <td key={columnIndex} className="font-semibold">
+                    {rowData[col.value]}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
@@ -75,4 +76,3 @@ function WorkersList({ column, data }) {
 }
 
 export default WorkersList;
-  
